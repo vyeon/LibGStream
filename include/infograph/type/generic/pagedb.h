22235bbd790ec32a87abc6568d32cfe6078c45b7
vee <vee@nullptr.kr>
2016-11-22 17:53:10 +0900

    Templatized pagedb class

--- include/infograph/type/generic/pagedb.h
+++ include/infograph/type/generic/pagedb.h
@@ -1,7 +1,7 @@
-#ifndef _INFOGRAPH_PAGEDB_H_
-#define _INFOGRAPH_PAGEDB_H_
+#ifndef _INFOGRAPH_TYPE_GENERIC_PAGEDB_H_
+#define _INFOGRAPH_TYPE_GENERIC_PAGEDB_H_
 
-#include <cstdint>
+#include <infograph/type/generic/slotted_page.h>
 
 namespace igraph {
 
@@ -18,6 +18,23 @@ public:
     bool read_from_file(const char* filepath);
 
     uint64_t num_pages = 0;
+};
+
+template <typename PAGE_T, 
+          template <typename ELEM_T,
+                    typename = std::allocator<ELEM_T> >
+                    class CONT_T = std::vector >
+class pagedb
+{
+public:
+    using page_t = PAGE_T;
+
+    // constructor
+    pagedb();
+
+    inline pagedb_info& info();
+protected:
+    pagedb_info _info;
 };
 
 
---
+++ src/type/generic/pagedb/pagedb_info.cpp
@@ -0,0 +1,18 @@
+#include <infograph/type/generic/pagedb.h>
+#include <fstream>
+
+namespace igraph {
+
+pagedb_info::pagedb_info(const char* filepath)
+{
+    read_from_file(filepath);
+}
+
+bool pagedb_info::read_from_file(const char* filepath) 
+{
+    std::ifstream ifs{ filepath, std::ios::in | std::ios::binary };
+    ifs.read(reinterpret_cast<char*>(&num_pages), sizeof(decltype(this->num_pages)));
+    return true;
+}
+
+} // !namespace igraph
